24202c16683bc2bf519f68865f088b19b81f9464
daniela <farias.danicris@gmail.com>
2021-07-16 21:21:00 -0300

    Criação da Header

---
+++ components/loginHeader.js
@@ -0,0 +1,13 @@
+
+import styles from '../styles/loginHeader.module.scss';
+
+export default function LoginHeader() {
+  return (
+        <header className={styles.header}>
+            
+
+        </header>
+
+        
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import WelcomeHome from '../components/WelcomeHome';
+import Login from './login'
 
 export default function Home() {
   return (
@@ -10,7 +10,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <WelcomeHome />
+     
 
     </div>
   )
