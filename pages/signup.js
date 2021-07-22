ca79cdbbb5d68f6af87f3e18584b9990758e5a48
Daniela <farias.danicris@gmail.com>
2021-07-22 20:32:12 -0300

    Api

--- pages/signup.js
+++ pages/signup.js
@@ -9,6 +9,7 @@ import deepPurple from '@material-ui/core/colors/deepPurple';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import Api from '../api/Api';
+import { Form } from 'react-bootstrap';
 
 export default function SignUp() {
 
@@ -66,8 +67,6 @@ export default function SignUp() {
         
     }
 
-    const corzinha = deepPurple[500];
-
 
     return (
         <div className={styles.SignUp__Page}>
@@ -79,131 +78,141 @@ export default function SignUp() {
             <header>
                 <Header pageName='Saudações, viajante! ' />
             </header>
+
+            
             <main>
-                <Container className={styles.teste} maxWidth={"xs"} spacing={5}>
-                    <Grid container direction="column" justifyContent='center' alignItems='center' fullWidth>
-                        <Grid xs={8} sm={4} item className={styles.SignUp__Introduction}>
-                            <Typography span='true' className={styles.SignUp__Introduction} variant="h5">
-                                Este é o universo Purple.
-                                <br />
-                                E quem é você?
-                            </Typography>
-                        </Grid>
-                        <Grid  xs={8} sm={4} item className={styles.SignUp__Image} >
-                            <img src="/new_images/persoicones-10.svg" alt="A letter image." />
-                        </Grid>
+                <Form onSubmit={this.submitHandler}>
+                    <Container className={styles.teste} maxWidth={"xs"} spacing={5}>
+                        <Grid container direction="column" justifyContent='center' alignItems='center' fullWidth>
+                            <Grid xs={8} sm={4} item className={styles.SignUp__Introduction}>
+                                <Typography span='true' className={styles.SignUp__Introduction} variant="h5">
+                                    Este é o universo Purple.
+                                    <br />
+                                    E quem é você?
+                                </Typography>
+                            </Grid>
+                            <Grid  xs={8} sm={4} item className={styles.SignUp__Image} >
+                                <img src="/new_images/persoicones-10.svg" alt="A letter image." />
+                            </Grid>
 
-                        <Grid xs={8} sm={4} item className={styles.SignUp__TextField} >
-                            <TextField
-                                required
-                                id={styles.SignUp__TextFieldContent}
-                                label="Nome Completo"
-                                InputProps={{ disableUnderline: (true) }}
-                                variant="standard"
-                                fullWidth
-                                type="text"
-                            />
-                        </Grid>
-
-                        <Grid xs={8} sm={4} item className={styles.SignUp__TextField} >
-                            <TextField
-                                required
-                                id={styles.SignUp__TextFieldContent}
-                                label="Nome de Usuário"
-                                InputProps={{ disableUnderline: (true) }}
-                                variant="standard"
-                                fullWidth
-                                type="text"
-                            />
-                        </Grid>
-
-                        <Grid xs={8} sm={4} item className={styles.SignUp__TextField}>
-                            <TextField
-                                required
-                                id={styles.SignUp__TextFieldContent}
-                                label="Email"
-                                InputProps={{ disableUnderline: (true) }}
-                                variant="standard"
-                                fullWidth
-                                type="email"
-                            />
-                        </Grid>
-
-                        <Grid xs={8} sm={4} item className={styles.SignUp__TextField}>
-                            <TextField
-                                required
-                                id={styles.SignUp__TextFieldContent}
-                                label="Confirme seu Email"
-                                InputProps={{ disableUnderline: (true) }}
-                                variant="standard"
-                                fullWidth
-                                type="email"
-                            />
-                        </Grid>
-
-                        <Grid xs={8} sm={4} item className={styles.SignUp__TextField}>
-                            <TextField
-                                required
-                                className={styles.SignUp__TextFieldContent}
-                                id={styles.SignUp__TextFieldContent}
-                                label="Data de Nascimento" InputProps={{ disableUnderline: true }}
-                                InputProps={{
-                                    disableUnderline: true,
-                                }}
-                                fullWidth
-                                type="date"
-                                defaultValue="2021-08-13"
-                                className={styles.SignUp__TextFieldContent}
-                                InputLabelProps={{
-                                    shrink: true,
-                                }}
-                            />
-                        </Grid>
-
-                        <Grid xs={8} sm={4} item className={styles.SignUp__TextField}>
-                            <FormControl id={styles.SignUp__TextField2} fullWidth>
-
-                                <InputLabel id={styles.SignUp__TextField2} >Senha</InputLabel>
-                                <Input
-                                    id={styles.SignUp__TextFieldContent2}
-                                    type={values.showPassword ? 'text' : 'password'}
-                                    value={values.password}
-                                    onChange={handleChange('password')}
-                                    controlId='passwordHash'
-                                    disableUnderline="true"
-                                    endAdornment={
-                                        <InputAdornment position="end">
-                                            <IconButton
-                                                color='success'
-                                                style={{ color: deepPurple[500] }}
-                                                aria-label="toggle password visibility"
-                                                onClick={handleClickShowPassword}
-                                                onMouseDown={handleMouseDownPassword}
-                                            >
-                                                {values.showPassword ? <Visibility /> : <VisibilityOff />}
-                                            </IconButton>
-                                        </InputAdornment>
-                                    }
+                            <Grid xs={8} sm={4} item className={styles.SignUp__TextField} >
+                                <TextField
+                                    required
+                                    id={styles.SignUp__TextFieldContent}
+                                    label="Nome Completo"
+                                    InputProps={{ disableUnderline: (true) }}
+                                    variant="standard"
+                                    fullWidth
+                                    type="text"
                                 />
+                            </Grid>
 
+                            <Form.Group controlId='userName'>
+                                <Grid xs={8} sm={4} item className={styles.SignUp__TextField} >
+                                    <TextField
+                                        required
+                                        id={styles.SignUp__TextFieldContent}
+                                        label="Nome de Usuário"
+                                        InputProps={{ disableUnderline: (true) }}
+                                        variant="standard"
+                                        fullWidth
+                                        type="text"
+                                    />
+                                </Grid>
+                            </Form.Group>
+
+                            <Grid xs={8} sm={4} item className={styles.SignUp__TextField}>
+                                <TextField
+                                    required
+                                    id={styles.SignUp__TextFieldContent}
+                                    label="Email"
+                                    InputProps={{ disableUnderline: (true) }}
+                                    variant="standard"
+                                    fullWidth
+                                    type="email"
+                                />
+                            </Grid>
+
+                            <Form.Group controlId='email'>
+                                <Grid xs={8} sm={4} item className={styles.SignUp__TextField}>
+                                    <TextField
+                                        required
+                                        id={styles.SignUp__TextFieldContent}
+                                        label="Confirme seu Email"
+                                        InputProps={{ disableUnderline: (true) }}
+                                        variant="standard"
+                                        fullWidth
+                                        type="email"
+                                    />
+                                </Grid>
+                            </Form.Group>
+
+                            <Grid xs={8} sm={4} item className={styles.SignUp__TextField}>
+                                <TextField
+                                    required
+                                    className={styles.SignUp__TextFieldContent}
+                                    id={styles.SignUp__TextFieldContent}
+                                    label="Data de Nascimento" InputProps={{ disableUnderline: true }}
+                                    InputProps={{
+                                        disableUnderline: true,
+                                    }}
+                                    fullWidth
+                                    type="date"
+                                    defaultValue="2021-08-13"
+                                    className={styles.SignUp__TextFieldContent}
+                                    InputLabelProps={{
+                                        shrink: true,
+                                    }}
+                                />
+                            </Grid>
+
+                            <Form.Group controlId='passwordHash'>
+                                <Grid xs={8} sm={4} item className={styles.SignUp__TextField}>
+                                    <FormControl id={styles.SignUp__TextField2} fullWidth>
+
+                                        <InputLabel id={styles.SignUp__TextField2} >Senha</InputLabel>
+                                        <Input
+                                            id={styles.SignUp__TextFieldContent2}
+                                            type={values.showPassword ? 'text' : 'password'}
+                                            value={values.password}
+                                            onChange={handleChange('password')}
+                                            controlId='passwordHash'
+                                            disableUnderline="true"
+                                            endAdornment={
+                                                <InputAdornment position="end">
+                                                    <IconButton
+                                                        color='success'
+                                                        style={{ color: deepPurple[500] }}
+                                                        aria-label="toggle password visibility"
+                                                        onClick={handleClickShowPassword}
+                                                        onMouseDown={handleMouseDownPassword}
+                                                    >
+                                                        {values.showPassword ? <Visibility /> : <VisibilityOff />}
+                                                    </IconButton>
+                                                </InputAdornment>
+                                            }
+                                        />
+                                    </FormControl>
+                                </Grid>
+                            </Form.Group>
+
+                            <Grid xs={8} sm={4} item>
+                                <Button
+                                    className={styles.SignUp__Button}
+                                    variant="primary"
+                                    href="/">
+                                    Aventurar-se
+                                </Button>
+                            </Grid>
                         </Grid>
 
-                        <Grid xs={8} sm={4} item>
-                            <Button
-                                className={styles.SignUp__Button}
-                                variant="primary"
-                                href="/">
-                                Aventurar-se
-                            </Button>
-                        </Grid>
-                    </Grid>
+                    </Container>
 
-                </Container>
-
-                <footer id={styles.SignUp__footer}>
-                    <Footer />
-                </footer>
-                );
+                    <footer id={styles.SignUp__footer}>
+                        <Footer />
+                    </footer>
+                    );
+                </Form>
             </main>
 
         </div >
