531df0812eede09863792bc0cb73ea22689c63b0
Marcos Domingues Junior <marcos_domingues@hotmail.com>
2021-07-22 21:43:30 -0300

    Welcome e respectivo scss padronizados

--- components/Welcome.js
+++ components/Welcome.js
@@ -1,8 +1,11 @@
 import Head from 'next/head';
+
 import Footer from '../components/Footer';
-import { Button } from 'react-bootstrap';
-import { Container, Grid, Typography } from '@material-ui/core';
 import styles from '../styles/welcome.module.scss';
+
+import { Button } from 'react-bootstrap';
+
+import { Container, Grid, Typography } from '@material-ui/core';
 
 export default function Welcome() {
     return (
@@ -16,8 +19,8 @@ export default function Welcome() {
                         <Grid item xs={12} sm={6}>
 
                             <Grid item className={styles.welcome__header}>
-                                <img src='new_images/persoicones-19.svg' className={styles.welcome__logo}/>
-                                <Typography variant="span" className={styles.welcome__title}>Purple</Typography>
+                                <img src='new_images/persoicones-19.svg' className={styles.welcome__header__logo}/>
+                                <Typography variant="span" className={styles.welcome__header__title}>Purple</Typography>
 
                             </Grid>
 
@@ -28,21 +31,21 @@ export default function Welcome() {
 
                             <Grid item className={styles.welcome__buttons}>
                                 <Grid item xs={12} sm={6}>
-                                    <Button href='/signup' className={styles.button__register} varian="primary">Inscrever-se</Button>
+                                    <Button href='/signup' className={styles.welcome__buttons__register} varian="primary">Inscrever-se</Button>
 
                                 </Grid>
 
                                 <Grid item xs={12} sm={6}>
-                                    <Button href='/login' className={styles.button__login} variant="primary">Entrar</Button>
+                                    <Button href='/login' className={styles.welcome__buttons__login} variant="primary">Entrar</Button>
                                 </Grid>
                             </Grid>
+
                         </Grid>
 
                         <Grid item xs={12} sm={6} className={styles.welcome__intro}>
                             <img src='new_images/persoicones-24.svg' />
-
                             <Typography variant="h2">Quem somos?</Typography>
-                            <Typography variant="p" className={styles.intro__text}>
+                            <Typography variant="p" className={styles.welcome__intro__text}>
                                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam nec dolor 
                                 ac nisi vehicula porttitor. Duis pharetra quam quis sapien fermentum 
                                 sodales. In commodo ex vel egestas euismod. Morbi scelerisque fringilla 
