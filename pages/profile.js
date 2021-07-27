358eefa4b13e2ebe03f7c2fd94a5caff38b79b66
Sulliman <johnatantc@hotmail.com>
2021-07-26 22:24:57 -0300

    Adição de elementos da página Profile, baseada nas informações do Figma

--- pages/profile.js
+++ pages/profile.js
@@ -1,10 +1,12 @@
 import Head from 'next/head';
-import { Container, Grid, Typography } from '@material-ui/core';
+import { Container, Grid, Typography, LinearProgress, Box } from '@material-ui/core';
+import { Button } from 'react-bootstrap';
+import FloatMenu from '../components/FloatMenu';
 import styles from '../styles/profile.module.scss';
 
-export default function Profile() {
+export default function Profile(props) {
 
-    return(
+    return (
 
         <div className={styles.profile}>
             <Head>
@@ -23,18 +25,37 @@ export default function Profile() {
                         </Grid>
                     </Grid>
 
-                    <Grid item>
-                        <Grid item className={styles.profile__charInfo}>
-                            <Typography className={styles.profile__charTitle}>Personagem</Typography>
-                            <img src='new_images/persoicones-23.svg' className={styles.profile__charImage} />
-                        </Grid>
+                    <Grid className={styles.profile__char}>
+                        <Typography className={styles.profile__charTitle}>Personagem</Typography>
+                        <img src='new_images/persoicones-23.svg' className={styles.profile__charImage} />
+                        <Grid item>
+                            <Grid item className={styles.profile__charLevel}>
+                                <Typography className={styles.profile__charTexts}>Level:</Typography>
+                                <Typography className={styles.profile__charTexts}>Rank:</Typography>
+                            </Grid>
 
-                        <Grid item className={styles.profile__bagdesInfo}>
-                            <Typography className={styles.profile__bagdesTitle}>Conquistas</Typography>
+                            <Grid>
+                                <Typography className={styles.profile__charTexts}>Exp:</Typography>
+                            </Grid>
+
+                            <Grid>
+                                <Typography className={styles.profile__charTexts}>Vigor:</Typography>
+                            </Grid>
+
+                            <Typography className={styles.profile__charTexts}>Ouro: 1000</Typography>
+                            <Button className={styles.profile__buttons} variant="primary">Editar Personagem</Button>
                         </Grid>
+                    </Grid>
+
+                    <Grid className={styles.profile__bagdes}>
+                        <Typography className={styles.profile__bagdesTitle}>Conquistas</Typography>
+                        <div>
+                            <Button className={styles.profile__buttons} variant="primary">Ver mais</Button>
+                        </div>
                     </Grid>
                 </Grid>
             </Container>
+            <FloatMenu />
         </div>
     );
 
