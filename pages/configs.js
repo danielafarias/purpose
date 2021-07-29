c028c078f217bf596169acc7f6e7e0f9b5516d5c
Guilherme de Carvalho Souza <gui_souza_123@hotmail.com>
2021-07-28 22:02:01 -0300

    editable buttons

--- pages/configs.js
+++ pages/configs.js
@@ -8,6 +8,47 @@ import { Grid, TextField, Typography, FormControl, InputLabel, Input, IconButton
 export default function Configs() {
 
     const [valor, setValor] = React.useState("")
+    const [valor1, setToggle1] = React.useState(false)
+    const [valor2, setToggle2] = React.useState(false)
+    const [valor3, setToggle3] = React.useState(false)
+
+    function toogle() {   
+        if (valor1 == true) {
+            document.getElementById('aaa').setAttribute('readonly', true)
+            setToggle1(false)
+        } else {
+            document.getElementById('aaa').removeAttribute('readonly')
+            setToggle1(true)
+        }
+
+        if (valor2 == true) {
+            document.getElementById('bbb').setAttribute('readonly', true)
+            setToggle2(false)
+        } else {
+            document.getElementById('bbb').removeAttribute('readonly')
+            setToggle2(true)
+        }
+    }
+
+    function toogle2() {   
+        if (valor2 == true) {
+            document.getElementById('bbb').setAttribute('readonly', true)
+            setToggle2(false)
+        } else {
+            document.getElementById('bbb').removeAttribute('readonly')
+            setToggle2(true)
+        }
+    }
+
+    function toogle3() {   
+        if (valor3 == true) {
+            document.getElementById('ccc').setAttribute('readonly', true)
+            setToggle3(false)
+        } else {
+            document.getElementById('ccc').removeAttribute('readonly')
+            setToggle3(true)
+        }
+    }
 
      return (
          <Grid className={styles.background}>
@@ -22,17 +63,21 @@ export default function Configs() {
                     onClick={() => setValor(document.getElementById("texto").value)}
                 >aaa</button>
             </Grid>
-
-            <div id="container">
-                <p id="edit"><b> Click "Edit" button to edit content. </b></p>
-                <button type="submit" id="edit-button">Edit</button>
-                <button type="submit" id="end-editing">Done</button>
-            </div>
-             
+            <Grid>
+            <button type="submit" className={styles.buttons} id="end-editing" onClick={() => toogle()}></button>
+            <input id='aaa' className={styles.inputs} type="text" defaultValue='Usuário' readonly='true'  class="field left"></input>  
+            </Grid>
+            <Grid>
+            <button type="submit" className={styles.buttons} id="end-editing" onClick={() => toogle2()}></button>
+            <input id='bbb' className={styles.inputs} type="text" defaultValue='Senha' readonly='true'  class="field left"></input>  
+            </Grid>
+            <Grid>
+            <button type="submit" className={styles.buttons} id="end-editing" onClick={() => toogle3()}></button>
+            <input id='ccc' className={styles.inputs} type="text" defaultValue='Teste@hotmail.com' readonly='true'  class="field left"></input>  
+            </Grid>
              <footer>
                  <FloatMenu/>
-             </footer>
-            
+             </footer>         
          </Grid>
      )
  }
