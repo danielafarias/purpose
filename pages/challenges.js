b1c9f750e193a654b10e880c9d5b5cd762451263
Daniela <farias.danicris@gmail.com>
2021-08-05 22:34:24 -0300

    Axios Auth

--- pages/challenges.js
+++ pages/challenges.js
@@ -1,16 +1,7 @@
 import React from 'react';
-import { withStyles } from '@material-ui/core/styles';
 import LoginHeader from '../components/Header';
 import styles from '../styles/challenges.module.scss'
-import { Grid, Box, TextField } from '@material-ui/core'
-import Button from '@material-ui/core/Button';
-import MenuItem from '@material-ui/core/MenuItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import DraftsIcon from '@material-ui/icons/Drafts';
-import SendIcon from '@material-ui/icons/Send';
-import Menu from '@material-ui/core/Menu';
+import { Grid } from '@material-ui/core'
 import FloatMenu from '../components/FloatMenu';
 
 export default function challenges() {
@@ -20,45 +11,47 @@ export default function challenges() {
             <header className={styles.header}>
                     <LoginHeader pageName='Desafios'/>
             </header>
-            <Grid className={styles.emailConfirmation} item xs={8} sm={4}>
-                <div className={styles.challengeBox}>
-                <div className={styles.container}></div>
-                <img className={styles.progIcon} src='/new_images/redes-03.svg'/>
-                <div className={styles.challengeTitle}>Título</div>  
-                <div className={styles.challengeDescription}>Pequasdkashdujuashduashdasudhs uashduashdsaudhsaudhsu uashdasuhdena descrição do negócio</div>              
-                </div> 
-            </Grid>
-            <Grid className={styles.emailConfirmation} item xs={8} sm={4}>
-                <div className={styles.challengeBox}>
-                <div className={styles.container}></div>
-                <img className={styles.progIcon} src='/new_images/redes-03.svg'/>
-                <div className={styles.challengeTitle}>Título</div>  
-                <div className={styles.challengeDescription}>Pequasdkashdujuashduashdasudhs uashduashdsaudhsaudhsu uashdasuhdena descrição do negócio</div>              
-                </div> 
-            </Grid>
-            <Grid className={styles.emailConfirmation} item xs={8} sm={4}>
-                <div className={styles.challengeBox}>
-                <div className={styles.container}></div>
-                <img className={styles.progIcon} src='/new_images/redes-03.svg'/>
-                <div className={styles.challengeTitle}>Título</div>  
-                <div className={styles.challengeDescription}>Pequasdkashdujuashduashdasudhs uashduashdsaudhsaudhsu uashdasuhdena descrição do negócio</div>              
-                </div> 
-            </Grid>
-            <Grid className={styles.emailConfirmation} item xs={8} sm={4}>
-                <div className={styles.challengeBox}>
-                <div className={styles.container}></div>
-                <img className={styles.progIcon} src='/new_images/redes-03.svg'/>
-                <div className={styles.challengeTitle}>Título</div>  
-                <div className={styles.challengeDescription}>Pequasdkashdujuashduashdasudhs uashduashdsaudhsaudhsu uashdasuhdena descrição do negócio</div>              
-                </div> 
-            </Grid>
-            <Grid className={styles.emailConfirmation} item xs={8} sm={4}>
-                <div className={styles.challengeBox}>
-                <div className={styles.container}></div>
-                <img className={styles.progIcon} src='/new_images/redes-03.svg'/>
-                <div className={styles.challengeTitle}>Título</div>  
-                <div className={styles.challengeDescription}>Pequasdkashdujuashduashdasudhs uashduashdsaudhsaudhsu uashdasuhdena descrição do negócio</div>              
-                </div> 
+            <Grid Container>
+                <Grid className={styles.emailConfirmation} item xs={8} sm={4}>
+                    <div className={styles.challengeBox}>
+                    <div className={styles.container}></div>
+                    <img className={styles.progIcon} src='/new_images/redes-03.svg'/>
+                    <div className={styles.challengeTitle}>Título</div>  
+                    <div className={styles.challengeDescription}>Pequasdkashdujuashduashdasudhs uashduashdsaudhsaudhsu uashdasuhdena descrição do negócio</div>              
+                    </div> 
+                </Grid>
+                <Grid className={styles.emailConfirmation} item xs={8} sm={4}>
+                    <div className={styles.challengeBox}>
+                    <div className={styles.container}></div>
+                    <img className={styles.progIcon} src='/new_images/redes-03.svg'/>
+                    <div className={styles.challengeTitle}>Título</div>  
+                    <div className={styles.challengeDescription}>Pequasdkashdujuashduashdasudhs uashduashdsaudhsaudhsu uashdasuhdena descrição do negócio</div>              
+                    </div> 
+                </Grid>
+                <Grid className={styles.emailConfirmation} item xs={8} sm={4}>
+                    <div className={styles.challengeBox}>
+                    <div className={styles.container}></div>
+                    <img className={styles.progIcon} src='/new_images/redes-03.svg'/>
+                    <div className={styles.challengeTitle}>Título</div>  
+                    <div className={styles.challengeDescription}>Pequasdkashdujuashduashdasudhs uashduashdsaudhsaudhsu uashdasuhdena descrição do negócio</div>              
+                    </div> 
+                </Grid>
+                <Grid className={styles.emailConfirmation} item xs={8} sm={4}>
+                    <div className={styles.challengeBox}>
+                    <div className={styles.container}></div>
+                    <img className={styles.progIcon} src='/new_images/redes-03.svg'/>
+                    <div className={styles.challengeTitle}>Título</div>  
+                    <div className={styles.challengeDescription}>Pequasdkashdujuashduashdasudhs uashduashdsaudhsaudhsu uashdasuhdena descrição do negócio</div>              
+                    </div> 
+                </Grid>
+                <Grid className={styles.emailConfirmation} item xs={8} sm={4}>
+                    <div className={styles.challengeBox}>
+                    <div className={styles.container}></div>
+                    <img className={styles.progIcon} src='/new_images/redes-03.svg'/>
+                    <div className={styles.challengeTitle}>Título</div>  
+                    <div className={styles.challengeDescription}>Pequasdkashdujuashduashdasudhs uashduashdsaudhsaudhsu uashdasuhdena descrição do negócio</div>              
+                    </div> 
+                </Grid>
             </Grid>
             <footer>
                 <FloatMenu/>
